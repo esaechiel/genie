bcec563f95ef300ce057fc0f9d675c0423329bbe
Sahil Arora <sahil.arora.8055@gmail.com>
2025-05-13 20:48:39 +0530

    Cleaned up untracked files/folders.

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  sendCaptchaSolved: (captcha) => ipcRenderer.send('captcha-solved', captcha),
-});
